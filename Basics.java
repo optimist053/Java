6bb9abe947a744ebdaf281a54bdbf8026c848495
Pankaj Kumar <optimistpankaj053@gmail.com>
2023-10-08 16:58:57 +0530

    Update Basics.java

--- Basics.java
+++ Basics.java
@@ -164,7 +164,65 @@ public class Basics{
     }
 } */
 
-//9. 
+/*9. Write a program in java to find the area of parellelogram.
+
+import java.util.*;
+
+public class Basics{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the base of the parallelogram ");
+        float base = in.nextFloat();
+
+        System.out.println("Enter the height of the parallelogram ");
+        float height =in.nextFloat();
+
+        float area = base * height;
+
+        System.out.println("The area of the parrellogram is "+area);
+    }
+} */
+
+/*10. Write a program to find the perimeter of circle .
+
+import java.util.*;
+
+public class Basics{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the radius of the circle ");
+        float radius = in.nextFloat();
+
+        double perimeter = 2*3.14*radius ;
+
+        System.out.println("Perimeter of the circle is "+perimeter);
+    }
+} */
+
+//11. Write a program to find the perimter of triangle 
+
+import java.util.*;
+
+public class Basics{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the first side of triangle");
+        float a = in.nextFloat();
+
+        System.out.println("Enter the second side of triangle ");
+        float b = in.nextFloat();
+
+        System.out.println("Enter the third side of triangle ");
+        float c = in.nextFloat();
+
+        double perimeter = a+b+c;
+
+        System.out.println("The perimeter of the triangle is "+perimeter);
+    }
+}
 
 
 
